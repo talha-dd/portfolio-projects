366aa49507f0ce88f88e3449d739c88dac182b3f
talha-dd <mtalha.dd@gmail.com>
2022-07-28 13:02:00 -0600

    updated favicons and titles

--- Restaurant Menu App/src/App.js
+++ Restaurant Menu App/src/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Menu from "./Menu";
-import Categories from "./Categories";
-import items from "./data";
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === "all") {
+    if (category === 'all') {
       setMenuItems(items);
       return;
     }
@@ -20,10 +20,10 @@ function App() {
   };
   return (
     <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Menu</h2>
+          <div className='underline'></div>
         </div>
 
         <Categories categories={categories} filterItems={filterItems} />
