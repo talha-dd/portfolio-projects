f1ab24c9eefe6d39d53b0a49e2bc997bcf56b911
talha-dd <mtalha.dd@gmail.com>
2022-07-27 21:08:17 -0600

    uploaded new projects, deleted old ones

--- image search api/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID emsx5kUZvCovHY8ePkHW4U-0VE1bwoL7ybHWpZ3JEmI",
-  },
-});
