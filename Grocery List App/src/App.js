5e132f40e0af6123c7b62258101eae2e2e0efeda
talha-dd <mtalha.dd@gmail.com>
2022-07-28 12:49:01 -0600

    updated favicon and name

--- Grocery List App/src/App.js
+++ Grocery List App/src/App.js
@@ -76,7 +76,7 @@ function App() {
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>Grocery Bud</h3>
+        <h3>Grocery List</h3>
         <div className='form-control'>
           <input
             type='text'
